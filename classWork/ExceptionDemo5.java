0cd4d6a79fbe70cbc24b0c11379cf7b83274d352
Payal Dhanwate <dhanwatepayal@gmail.com>
2025-12-12 21:45:43 +0530

    Add files via upload

---
+++ classWork/ExceptionDemo5.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class ExceptionDemo5
+{
+    public static void main(String A[]) 
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iNo1=0 ,  iNo2=0 ,iAns = 0;
+        
+
+        System.out.println("Enter firest number :");
+        iNo1 = sobj.nextInt();
+
+        System.out.println("Enter second number :");
+        iNo2 = sobj.nextInt();
+        
+        try
+        {
+           System.out.println("Inside try Block");
+           iAns = iNo1 / iNo2;
+        }
+
+        catch(ArithmeticException aobj)      //specific catch
+        {
+            System.out.println("Inside catch  Block");
+            System.out.println(aobj);
+
+        }
+        catch(Exception eobj)     //generic catch
+        {
+            System.out.println("Inside generic catch ");
+        }
+
+        System.out.println("Division is :"+iAns);
+
+
+
+    }
+}
